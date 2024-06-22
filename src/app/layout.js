2e3642b40597167a8eb4b44cf7286efa2bd30495
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-06-22 14:23:54 +0200

    update admin logout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,22 @@
 import { GoogleAnalytics } from "@next/third-parties/google";
 import Header from "@/components/header/Header";
+import AuthProvider from "@/components/AuthProvider";
 import Footer from "@/components/footer/Footer";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="es-ES">
-      <body>
+      <AuthProvider>
+        <body>
           <Header />
           <main>
             {children}
             <GoogleAnalytics gaId="G-S8PFRPB6DC" />
           </main>
           <Footer />
-      </body>
+        </body>
+      </AuthProvider>
     </html>
   );
 }
--- src/components/header/NavMenu/NavMenu.jsx
+++ src/components/header/NavMenu/NavMenu.jsx
@@ -2,21 +2,30 @@
 
 import React from "react";
 import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 import { links } from "@/app/lib/linksMenu";
 import LinkHeader from "./LinkHeader/LinkHeader";
 import css from "./NavMenu.module.css";
 
 const NavMenu = () => {
-const pathname = usePathname();
+  const { data: session } = useSession();
+  const pathname = usePathname();
 
   return (
     <div>
       <ul className={css.listMenu}>
         {links.map((link) => (
-          <li key={link.name} className={pathname === link.path ? css.activeItem : css.listItem}>
+          <li
+            key={link.name}
+            className={pathname === link.path ? css.activeItem : css.listItem}
+          >
             <LinkHeader link={link} />
           </li>
         ))}
+        {session && (
+            <Link href={"/api/auth/signout?callbackUrl=/"}>Logout</Link>
+          )}
       </ul>
     </div>
   );
