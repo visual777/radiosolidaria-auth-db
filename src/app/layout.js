e9c963ddcbd37525ff1e6ed06e213bbc80f93b30
Vladyslav Vagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-06-23 21:39:01 +0200

    splash screen

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,41 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import Header from "@/components/header/Header";
 import AuthProvider from "@/components/AuthProvider";
 import Footer from "@/components/footer/Footer";
+import FirstScreen from "@/components/FirstScreen/FirstScreen";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <html lang="es-ES">
       <AuthProvider>
         <body>
-          <Header />
-          <main>
-            {children}
-            <GoogleAnalytics gaId="G-S8PFRPB6DC" />
-          </main>
-          <Footer />
+          {loading ? (
+            <FirstScreen />
+          ) : (
+            <>
+              {" "}
+              <Header />
+              <main>
+                {children}
+                <GoogleAnalytics gaId="G-S8PFRPB6DC" />
+              </main>
+              <Footer />
+            </>
+          )}
         </body>
       </AuthProvider>
     </html>
---
+++ src/components/FirstScreen/FirstScreen.jsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import Image from "next/image";
+import css from "./FirstScreen.module.css";
+
+const FirstScreen = () => {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress((oldProgress) => {
+        if (oldProgress === 100) {
+          clearInterval(timer);
+        }
+        const diff = 100 / (5 * 10); // 5 seconds, 10 for 100ms interval
+        return Math.min(oldProgress + diff, 100);
+      });
+    }, 100);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  return (
+    <div className={css.containerImage}>
+      <Image
+        src="/images/logo/logo-aquamarin.webp"
+        alt="Bienvenido a Solidaria Media"
+        width={300}
+        height={222}
+        className={css.image}
+      />
+      <div className={css.progressBar}>
+        <div className={css.progress} style={{ width: `${progress}%` }} />
+      </div>
+    </div>
+  );
+};
+
+export default FirstScreen;
