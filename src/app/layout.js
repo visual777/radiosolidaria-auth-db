98711a2cc4123001f11af102e490a662286e95d4
VladyslavVagin <140642012+VladyslavVagin@users.noreply.github.com>
2024-06-24 16:36:55 +0200

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,13 +2,13 @@
 
 import { useState, useEffect } from "react";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import { Analytics } from "@vercel/analytics/react"
-import { ToastContainer } from 'react-toastify';
+import { Analytics } from "@vercel/analytics/react";
+import { ToastContainer } from "react-toastify";
 import Header from "@/components/header/Header";
 import AuthProvider from "@/components/AuthProvider";
 import Footer from "@/components/footer/Footer";
 import FirstScreen from "@/components/FirstScreen/FirstScreen";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
@@ -36,7 +36,18 @@ export default function RootLayout({ children }) {
                 {children}
                 <GoogleAnalytics gaId="G-S8PFRPB6DC" />
                 <Analytics />
-                <ToastContainer />
+                <ToastContainer
+                  position="top-right"
+                  autoClose={5000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  closeOnClick
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover
+                  theme="dark"
+                />
               </main>
               <Footer />
             </>
