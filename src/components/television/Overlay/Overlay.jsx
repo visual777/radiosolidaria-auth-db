39f916d952aab0635dd970333f90e52fba342848
visual777 <ismael@visual777.com>
2024-06-26 18:45:40 +0200

    update tv page
    update overlay, adding icon Click

--- src/components/television/Overlay/Overlay.jsx
+++ src/components/television/Overlay/Overlay.jsx
@@ -1,9 +1,11 @@
+import Icon from "@/components/Icon/Icon";
 import css from "./Overlay.module.css";
 
 const Overlay = () => {
   return (
     <div className={css.overlay}>
         <p className={css.text}>Haz click en España o Argentina para reproducir</p>
+        <Icon name='icon-one-finger-click' className={css.icon}/>
     </div>
   );
 };
