268c222005354c746b65d47f462ed693bc38338a
visual777 <ismael@visual777.com>
2024-07-02 16:08:46 +0200

    change link of radio

--- src/app/radio/page.jsx
+++ src/app/radio/page.jsx
@@ -34,7 +34,7 @@ const page = () => {
       <div className={css.container}>
         <div className={css.heroContainer}>
           <DesktopImg />
-          <AudioRadio url="http://master.shoutcast.com:8000/radiosolidaria" />
+          <AudioRadio url="https://canadaremar2.todostreaming.es/live/solidariatv-radio.m3u8" />
         </div>
         <div className={css.contentContainer}>
           <LastVideos />
