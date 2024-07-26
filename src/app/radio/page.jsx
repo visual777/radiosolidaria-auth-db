f58b05ee98fca8b19cda1f9827b718e5b0bd6aa1
visual777 <ismael@visual777.com>
2024-07-26 13:26:55 +0200

    link viejo

--- src/app/radio/page.jsx
+++ src/app/radio/page.jsx
@@ -34,7 +34,7 @@ const page = () => {
       <div className={css.container}>
         <div className={css.heroContainer}>
           <DesktopImg />
-          <AudioRadio url="https://ais-edge92-lax02.cdnstream.com/2634_128.mp3"/>
+          <AudioRadio url="https://master.shoutcast.com:8000/radiosolidaria"/>
         </div>
         <div className={css.contentContainer}>
           <LastVideos />
